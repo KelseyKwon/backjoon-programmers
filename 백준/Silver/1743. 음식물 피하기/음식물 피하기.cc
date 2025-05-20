d11e98cfba343f312d72df9a356f607418cddd8a
KelseyKwon <nykwon7777@gmail.com>
2025-05-20 21:48:16 +0900

    [revised] 기존 코드도 넣기

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.cc
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.cc
@@ -1,3 +1,63 @@
+/*
+// 세로 길이 N, 가로 길이 M, 음식물 쓰레기 개수 K
+// 
+
+// vector<pair<int, int>> 로 저장하고
+// 상하좌우 확인해서 있으면 -> dfs 아니면 ... 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>   // for std::find
+
+using namespace std;
+
+int N, M, K;
+vector<bool> visited;
+vector<pair<int, int>> trash;
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, -1, 0, 1};
+
+bool isInRightRange(int x, int y) {
+    return (x >= 0 && x < N && y >= 0 && y < M);
+}
+
+int dfs(int x, int y, int size) {
+    visited[x+y-1] = true;
+
+    for (int i=0; i<4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if (!visited[nx+ny-1] &&
+            isInRightRange(nx, ny) &&
+            find(trash.begin(), trash.end(), make_pair(nx, ny)) != trash.end()) {
+                dfs(nx, ny, size++);
+            }
+    }
+    return size;
+}
+
+int main() {
+    cin >> N >> M >> K;
+    visited.resize(N*M, false);
+
+    for (int i=0; i<K; i++) {
+        int x, y;
+        cin >> x >> y;
+
+        trash.push_back({x, y});
+    }
+
+    int answer = 0;
+    for (int i=0; i<trash.size(); i++) {
+        int cur_max_size = dfs(trash[i].first, trash[i].second, 1);
+        answer = max(answer, cur_max_size);
+    }
+    cout << answer;
+
+}
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -53,3 +113,4 @@ int main() {
     cout << answer;
     return 0;
 }
+
