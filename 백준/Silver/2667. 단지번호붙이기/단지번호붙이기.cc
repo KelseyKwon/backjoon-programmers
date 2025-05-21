1c96b73ab03558b36a9f4af594697475a8acc6e2
KelseyKwon <nykwon7777@gmail.com>
2025-05-21 15:54:24 +0900

    [틀린 version 추가]

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,3 +1,61 @@
+// // 좌우, 아래위로 있으면 연결됨.
+// // 만약에 상하좌우가 1이면 -> count++하고 그 곳으로 가서 상화좌우가 1...
+// // dfs로 계속 부름
+
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// int N, count;
+// vector<vector<int>> graph;
+// vector<vector<bool>> visited;
+
+// int dx[] = {0, 1, 0, -1};
+// int dy[] = {1, 0, -1, 0};
+
+// int dfs(int start, int end) {
+//   for (int i=start; i<N; i++) {
+//     for (int j=end; j<N; j++) {
+//       if (graph[i][j] == 1) {
+//         count++;
+//         visited[i][j] = true;
+//         for (int i=0; i<4; i++) {
+//           int ni = i + dx[i];
+//           int nj = j + dx[j];
+
+//           if (graph[ni][nj] == 1 && !visited[ni][nj]) {
+//             dfs(ni, nj);
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+
+// int main() {
+//   cin >> N;
+//   graph.resize(N, vector<int>(N));
+//   visited.resize(N, vector<bool>(N, false));
+//   vector<int> house_count;
+
+//   for (int i=0; i<N; i++) {
+//     for (int j=0; j<N; j++) {
+//       cin >> graph[i][j];
+//     }
+//   }
+
+//   for (int i=0; i<N; i++) {
+//     for (int j=0; j<N; j++) {
+//       count = 0;
+//       if (!visited[i][j] &&graph[i][j] == 1) {
+//         house_count.push_back(dfs(i, j));
+//       }
+//     }
+//   }
+
+// }
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -56,6 +114,11 @@ int main() {
     }
 
     sort(house_count.begin(), house_count.end());
+
+    cout << house_count.size() << '\n';
+    for (int h : house_count) cout << h << '\n';
+}
+
 
     cout << house_count.size() << '\n';
     for (int h : house_count) cout << h << '\n';
