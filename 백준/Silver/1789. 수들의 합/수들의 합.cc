7236b995503bab0a5fa399069ff8a7b031e9eda1
KelseyKwon <nykwon7777@gmail.com>
2025-05-23 21:59:38 +0900

    [기존 코드 추가]

--- 백준/Silver/1789. 수들의 합/수들의 합.cc
+++ 백준/Silver/1789. 수들의 합/수들의 합.cc
@@ -1,3 +1,30 @@
+/*
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    long long S;
+    cin >> S;
+
+    int sum = 0, count = 0;
+    for (int i=1; i<S;i++) {
+        if (sum + i == S) {
+            count++;
+            break;
+        } else if (sum + i < S) {
+            sum += i;
+            count++;
+        } else {
+            break;
+        }
+    }
+
+    cout << count;
+}
+*/
+
+
 #include <iostream>
 using namespace std;
 
