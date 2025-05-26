b70bf88281a417b15711c831ad3df844ee626516
KelseyKwon <nykwon7777@gmail.com>
2025-05-26 15:52:43 +0900

    [내 코드 추가]

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -1,3 +1,49 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(string s) {
+    vector<int> answer(2); // {회차, 제거한 0의 개수}
+    int zero_count = 0;
+    int round_count = 0;
+
+    while (s != "10") {
+      string temp = "";
+      round_count += 1;
+      // 0 제거
+      for (char c : s) {
+        if (c == '0') {
+          zero_count += 1;
+        } else {
+          temp += c;
+        }
+      }
+
+      // 2진법으로 변환
+      int cal_num = temp.size();
+      vector<int> results;
+
+      while (cal_num / 2 != 0) {
+        results.push_back(cal_num % 2);
+        cal_num /= 2;
+      } 
+      
+      s = "";
+      for (int i=results.size()-1; i>=0; i--) {
+        s += results[i];
+      }
+  
+    }
+    round_count += 1;
+    zero_count += 1;
+    answer[0] = round_count;
+    answer[1] = zero_count;
+
+    return answer;
+}
+    */
+
     #include <string>
     #include <vector>
     
@@ -27,3 +73,4 @@
         answer[1] = zero_count;
         return answer;
     }
+    
