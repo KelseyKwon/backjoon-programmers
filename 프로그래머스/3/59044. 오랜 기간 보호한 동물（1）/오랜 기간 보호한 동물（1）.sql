dad503296f19738c8d6f9d08b2bd6ba30f91efa6
KelseyKwon <nykwon7777@gmail.com>
2025-06-25 22:23:11 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+-- 입양 못보낸 것 (ANIMAL_INS에는 있으나 ANIMAL_OUTS에는 없음)
+-- 가장 오래 보호소에 있었던 (DATETIME이 가장 먼저 시작)
+-- 3마리의 이름과 보호 시작일
+SELECT a.NAME, a.DATETIME
+from ANIMAL_INS as a left join ANIMAL_OUTS as b 
+on a.ANIMAL_ID = b.ANIMAL_ID
+where b.ANIMAL_ID is null
+order by DATETIME asc
+limit 3
