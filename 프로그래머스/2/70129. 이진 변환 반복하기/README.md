0787c206ec04884cee7ae4dfa10a641829f23357
KelseyKwon <nykwon7777@gmail.com>
2025-07-02 15:31:40 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.90 ms, Memory: 9.45 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/README.md
+++ 프로그래머스/2/70129. 이진 변환 반복하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 이진 변환 반복하기 - 70129 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129?language=python3) 
 
 ### 성능 요약
 
-메모리: 4.22 MB, 시간: 0.86 ms
+메모리: 9.45 MB, 시간: 1.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 26일 15:51:03
+2025년 07월 02일 15:31:37
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -0,0 +1,42 @@
+# 이진 변환 횟구와 제거하는 0의 개수를 answer에 담아 반환하기
+
+# def search(str, answer):
+#     if len(str) == 1:
+#         return
+#     else: 
+#         answer[0] += str.count("0")
+#         str = str.replace("0", "")
+#         answer[1] += 1
+#         return search(str, answer)
+
+# def solution(s):
+#     answer = [0, 0]
+#     search(s, answer)
+#     return answer
+def search(str, answer):
+    if str == "1":
+        return answer
+    else: 
+        answer[1] += str.count("0")
+        str = str.replace("0", "")
+        answer[0] += 1
+        str = bin(len(str))[2:]
+        return search(str, answer)
+
+def solution(s):
+    answer = [0, 0]
+    return search(s, answer)
+
+# def dfs(s, transforms, zeros):
+#     if s == "1":
+#         return transforms, zeros
+    
+#     z = s.count("0")
+#     zeros += z
+#     s = s.replace("0", "")
+#     s = bin(len(s))[2:]
+#     return dfs(s, transforms+1, zeros)
+
+# def solution(s):
+#     t, z = dfs(s, 0, 0)
+#     return [t, z]
