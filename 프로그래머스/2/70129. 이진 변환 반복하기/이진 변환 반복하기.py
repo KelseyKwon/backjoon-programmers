293cf8e84d55d3b1bc8ee05c2abde5bb4caafe35
KelseyKwon <nykwon7777@gmail.com>
2025-07-03 13:49:26 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.98 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -13,19 +13,19 @@
 #     answer = [0, 0]
 #     search(s, answer)
 #     return answer
-def search(str, answer):
-    if str == "1":
-        return answer
-    else: 
-        answer[1] += str.count("0")
-        str = str.replace("0", "")
-        answer[0] += 1
-        str = bin(len(str))[2:]
-        return search(str, answer)
+# def search(str, answer):
+#     if str == "1":
+#         return answer
+#     else: 
+#         answer[1] += str.count("0")
+#         str = str.replace("0", "")
+#         answer[0] += 1
+#         str = bin(len(str))[2:]
+#         return search(str, answer)
 
-def solution(s):
-    answer = [0, 0]
-    return search(s, answer)
+# def solution(s):
+#     answer = [0, 0]
+#     return search(s, answer)
 
 # def dfs(s, transforms, zeros):
 #     if s == "1":
@@ -40,3 +40,16 @@ def solution(s):
 # def solution(s):
 #     t, z = dfs(s, 0, 0)
 #     return [t, z]
+
+def solution(s):
+    answer = [0, 0]  # [변환 횟수, 제거된 0의 개수]
+
+    while s != '1':
+        zero_count = s.count('0')
+        s = s.replace('0', '')  # 0 제거
+        length = len(s)
+        s = bin(length)[2:]  # 길이를 2진수로 변환
+        answer[0] += 1
+        answer[1] += zero_count
+
+    return answer
