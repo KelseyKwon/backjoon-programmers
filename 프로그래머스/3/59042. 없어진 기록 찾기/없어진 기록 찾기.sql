edcdf87ac757048a08045b06e112faa6ea7602f1
KelseyKwon <nykwon7777@gmail.com>
2025-07-04 20:04:00 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,6 +1,8 @@
 -- 코드를 입력하세요
--- 입양간 기록 기록, 보호소에 들어온 기록이 없는 것.
-SELECT a.ANIMAL_ID, a.NAME
-from ANIMAL_OUTS as a left outer join ANIMAL_INS as b
-on a.ANIMAL_ID = b.ANIMAL_ID
-where b.INTAKE_CONDITION is null
+-- ANIMAL_OUTS에 존재하는데, ANIMAL_INS에 없는 테이블
+-- order by ANIMAL_ID ANIMAL_OUTS.NAME
+SELECT b.ANIMAL_ID, b.NAME
+from ANIMAL_OUTS b 
+left outer join ANIMAL_INS a on a.ANIMAL_ID = b.ANIMAL_ID
+where a.ANIMAL_ID is null
+order by b.ANIMAL_ID
