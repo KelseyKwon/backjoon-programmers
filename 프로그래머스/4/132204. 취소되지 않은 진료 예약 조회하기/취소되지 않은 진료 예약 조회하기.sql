a7a017fcfedd6da60eced06f0b847ffeb5b5c8ea
KelseyKwon <nykwon7777@gmail.com>
2025-07-04 20:34:19 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,14 +1,14 @@
--- APNT_YMD = 2022-04-13 and APNT_CNCL_YN = No
--- MCDP_CD = 'CS'
--- c.APNT_NO, a.PT_NAME a.PT_NO b.MCDP_CD  b.DR_NAME a.APNT_YMD
-
-with no_canceled as (
+-- 코드를 입력하세요
+-- APNT_YMD = '2022-04-13' and APNT_CNCL_YN = 'N'
+-- c.APNT_NO, a.PT_NAME, a.PT_NO, c.MCDP_CD, b.DR_NAME, c.APNT_YMD
+-- c.APNT_YMD asc
+with not_canceled as (
     select *
     from APPOINTMENT
-    where DATE(APNT_YMD) = '2022-04-13' and APNT_CNCL_YN = 'N' and MCDP_CD = 'CS'
+    where DATE(APNT_YMD) = '2022-04-13' and APNT_CNCL_YN = 'N'
 )
-select c.APNT_NO, a.PT_NAME, a.PT_NO, b.MCDP_CD, b.DR_NAME, c.APNT_YMD
-from no_canceled c
+select c.APNT_NO, a.PT_NAME, a.PT_NO, c.MCDP_CD, b.DR_NAMe, c.APNT_YMD
+from not_canceled c
 join PATIENT a on c.PT_NO = a.PT_NO
 join DOCTOR b on c.MDDR_ID = b.DR_ID
 order by c.APNT_YMD
