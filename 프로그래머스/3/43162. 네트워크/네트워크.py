e73e94566c62528f5c42ad776d4b8c034d746ee5
KelseyKwon <nykwon7777@gmail.com>
2025-07-04 23:44:17 +0900

    [level 3] Title: 네트워크, Time: 0.80 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,17 +1,24 @@
 def solution(n, computers):
-  count = 0
-  visited = [False] * n
-
-  def dfs(node):
-    visited[node] = True
-
+    answer = 0
+    
+    def dfs(node):
+        visited[node] = True
+        for i in graph[node]:
+            if not visited[i]:
+                dfs(i)
+    
+    graph = [[] for _ in range(n)]
+    visited = [False] * n
+    
     for i in range(n):
-      if not visited[i] and computers[node][i]:
-        dfs(i)
-
-  for i in range(n):
-    if not visited[i]:
-      dfs(i)
-      count += 1
-
-  return count
+        for j in range(n):
+            if (i == j): 
+                continue
+            elif(computers[i][j] == 1):
+                graph[i].append(j)
+    for k in range(n):
+        if not visited[k]:
+            dfs(k)
+            answer += 1
+                
+    return answer
