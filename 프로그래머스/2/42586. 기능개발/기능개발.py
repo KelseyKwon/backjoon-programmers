b9e8ee229a54279f0d4c23352fd5a00dd265ccc3
KelseyKwon <nykwon7777@gmail.com>
2025-07-04 14:15:42 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,25 +1,27 @@
 import math
-from collections import deque
 
 def solution(progresses, speeds):
     answer = []
-    N = len(progresses)
-    queue = deque()
-
-    for i in range(N):
-      cur_num = math.ceil((100 - progresses[i]) / speeds[i])
-      queue.append(cur_num)
     
-    now_num = queue.popleft()
-    cnt = 1
-
-    while queue:
-      next_num = queue.popleft()
-      if (now_num >= next_num):
-        cnt += 1
-      else:
-        answer.append(cnt)
-        now_num = next_num
-        cnt = 1
-    answer.append(cnt)
+    # ceil((100 - 93) / 1)
+    # 7, 3, 9
+    # while 더 작을때까지 ++
+    
+    # 5, 10, 1, 1, 20, 1
+    days = []
+    
+    for i in range(len(progresses)):
+        # [7, 3, 9]
+        days.append(math.ceil((100 - progresses[i]) / speeds[i]))
+        
+    flag = days[0]
+    count = 1
+    for i in range(1, len(days)):
+        if flag < days[i]:
+            flag = days[i]
+            answer.append(count)
+            count = 1
+        else:
+            count += 1
+    answer.append(count)
     return answer
