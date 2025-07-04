b0a177fb97e98ae9e7acd4487a83487c3158c029
KelseyKwon <nykwon7777@gmail.com>
2025-07-04 20:54:56 +0900

    [level 4] Title: 년, 월, 성별 별 상품 구매 회원 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
+++ 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
@@ -1,15 +1,9 @@
 -- 코드를 입력하세요
--- GENDER는 0 or 1 or NULL
--- SALES_DATE, USER_ID, PRODUCT_ID 조합에 대해서는 판매 데이터가 하나만
--- SALES_DATE 년, 월, 성별을 기준을로 오름차순
--- GENdER가 NULL이면 제외하기
-with yes_gender as (
-select *
-from USER_INFO
-where GENDER is NOT NULL
-)
-select YEAR(SALES_DATE) as YEAR, MONTH(SALES_DATE) as MONTH, a.GENDER, count(DISTINCT a.USER_ID) as USERS
-from ONLINE_SALE b
-join yes_gender a on a.USER_ID = b.USER_ID
-group by YEAR(SALES_DATE), MONTH(SALES_DATE), a.GENDER
-order by YEAR(SALES_DATE) asc, MONTH(SALES_DATE) asc, GENDER asc
+-- group by YEAR(SALES_DATE), MONTh(SALES_DATE), GENDER, count(USER_ID)
+-- GENDER is not null
+-- year, month, gender을 기준으로 asc
+SELECT YEAR(b.SALES_DATE) as YEAR, MONTH(B.SALES_DATE) as MONTH, a.GENDER, count(distinct a.USER_ID) as USERS
+from USER_INFO a
+join ONLINE_SALE b on a.USER_ID = b.USER_ID
+where a.GENDER is not null
+group by YEAR(b.SALES_DATE), MONTH(B.SALES_DATE), a.GENDER
