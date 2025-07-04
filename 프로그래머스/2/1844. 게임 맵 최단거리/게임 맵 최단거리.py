96a482e55a02cd0e7878fa1db71a315d106bb081
KelseyKwon <nykwon7777@gmail.com>
2025-07-04 16:28:24 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.26 ms, Memory: 9.42 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,38 +1,38 @@
+"""
+    최단거리 -> bfs -> queue -> 2차원이니까 dx, dy 정의 , 배열에 count 더하기
+    """
 from collections import deque
 
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-
 def solution(maps):
-  rows = len(maps)
-  cols = len(maps[0])
-
-  visited = [[False] * cols for _ in range(rows)]
-
-  def inRange(x, y):
-    return 0 <= x < rows and 0 <= y < cols
-
-  def bfs(x, y):
-    q = deque()
-    visited[x][y] = True
-    q.append((x, y))
-
-    while q:
-      cur_x, cur_y = q.popleft()
-
-      if cur_x == rows-1 and cur_y == cols-1:
-        return maps[cur_x][cur_y]
-      
-      for i in range(4):
-        nx = cur_x + dx[i]
-        ny = cur_y + dy[i]
-
-        if inRange(nx, ny) and not visited[nx][ny] and maps[nx][ny] == 1:
-          visited[nx][ny] = True
-          q.append((nx, ny))
-          maps[nx][ny] += maps[cur_x][cur_y]
-    return -1
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
+    def inRange(x, y):
+        return 0<=x<n and 0<=y<m
     
-  answer = bfs(0, 0)
-  return answer
-
+    def bfs(x, y):
+        q = deque()
+        q.append((x, y, 1))
+        visited[x][y] = True
+        
+        while q:
+            cur_x, cur_y, dist = q.popleft()
+            if cur_x == n-1 and cur_y == m-1:
+                return dist
+            
+            for i in range(4):
+                nx = cur_x + dx[i]
+                ny = cur_y + dy[i]
+                
+                if inRange(nx, ny) and not visited[nx][ny] and maps[nx][ny] == 1:
+                    visited[nx][ny] = True
+                    q.append((nx, ny, dist+1))
+                    
+        return -1
+                    
+        
+    n = len(maps)
+    m = len(maps[0])
+    
+    visited = [[False] * m for _ in range(n)]
+    return bfs(0, 0)
+    
