06189f09cff4222f9e746db8b586b26f94a6ef74
KelseyKwon <nykwon7777@gmail.com>
2025-07-04 23:32:05 +0900

    [level 2] Title: 프로세스, Time: 4.08 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,16 +1,17 @@
 from collections import deque
 
 def solution(priorities, location):
-    answer = 0
-    q = deque([(p, idx) for idx, p in enumerate(priorities)])
-
+    count = 0
+    q = deque()
+    for index, v in enumerate(priorities):
+        q.append((v, index)) # (2, 0), (1, 1) , (3, 2) 이렇게
+        print(q)
+    
     while q:
-      cur = q.popleft()
-      if any(cur[0] < nxt[0] for nxt in q):
-        q.append(cur)
-      else:
-        answer += 1
-        if cur[1] == location:
-          return answer
-
-
+        cur_priority , index = q.popleft()
+        if any(k[0] > cur_priority for k in q):
+            q.append((cur_priority, index))
+        else:
+            count += 1
+            if index == location:
+                return count
