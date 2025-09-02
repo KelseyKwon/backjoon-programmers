9df6c3c87d48adcd174778d38a55c6936d998c6d
KelseyKwon <nykwon7777@gmail.com>
2025-09-02 12:29:38 +0900

    [level 2] Title: 방문 길이, Time: 0.42 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,26 +1,26 @@
-def valid_point(x, y):
-  return (0<=x<=10 and 0<=y<=10)
+"""
+중복 방지 -> set!
+set에 좌표를 담아 -> (시작, 끝) / (끝, 시작) 으로 
 
-def move_point(x, y, dir):
-  dx = [0, 1, 0, -1]
-  dy = [1, 0, -1, 0]
-  if dir == 'U':
-    return x + dx[0],  y + dy[0]
-  elif dir == 'R':
-    return x + dx[1],  y + dy[1]
-  elif dir == 'D':
-    return x + dx[2],  y + dy[2]
-  else:
-    return x + dx[3],  y + dy[3]
+예외 처리 : 범위 안에 있고, 그리고 set / 2을 반환해.
+"""
 
+def inRange(x, y):
+    return -5 <= x <= 5 and -5 <= y <= 5
 
 def solution(dirs):
-    answer = set()
-    x, y = 5, 5
-    for dir in dirs:
-      nx, ny = move_point(x, y, dir)
-      if (valid_point(nx, ny)):
-        answer.add((x, y, nx, ny))
-        answer.add((nx, ny, x, y))
-        x, y = nx, ny
-    return len(answer)//2
+    cur_x, cur_y = 0, 0
+    answers = set()
+    
+    moves = {'U': (0,1), 'D': (0,-1), 'R': (1,0), 'L': (-1,0)}
+    
+    for d in dirs:
+        dx, dy = moves[d]
+        nx, ny = cur_x + dx, cur_y + dy
+        if inRange(nx, ny):
+            answers.add(((cur_x, cur_y), (nx, ny)))
+            answers.add(((nx, ny), (cur_x, cur_y)))
+            cur_x, cur_y = nx, ny
+        
+    
+    return len(answers)//2
