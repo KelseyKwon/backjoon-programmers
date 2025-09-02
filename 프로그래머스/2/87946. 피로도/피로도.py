f10a992ef0587d08582a6c6de6b459c414bd5bb7
KelseyKwon <nykwon7777@gmail.com>
2025-09-02 12:16:27 +0900

    [level 2] Title: 피로도, Time: 40.13 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,19 +1,30 @@
-import itertools
+"""
+최소 필요 피로도 >= 소모 피로도
+
+순열 개수 만큼 나온다. 
+
+1) 1 2 3 -> 80이 60이 됨 -> 60이 20이 됨 -> 다음에 못함. 
+
+순열을 만들어서 순열 개수만큼 한다. 
+
+순열로 횟수를 만들어 -> 만약에 현재 k가 각 0번째보다 크거나 같으면 -> 계속해 그리고 횟수 +1 -> 작으면 -> 그 다음으로 바로 넘어가 -> 그리고 max 값을 반환해
+"""
+from itertools import permutations
 
 def solution(k, dungeons):
-    def checked(k, dungeon):
-        cur_fatigue = k
-        count = 0
-        for stage in dungeon:
-            if cur_fatigue >= stage[0]:
-                cur_fatigue -= stage[1]
-                count += 1
-            else:
-                return count
-        return count
     answer = 0
     
-    for element in itertools.permutations(dungeons):
-        answer = max(answer, checked(k, element))
-    
+    for perm in permutations(dungeons):
+        fatigue = k
+        temp = 0
+        for stage in perm:
+            if (fatigue >= stage[0]):
+                fatigue -= stage[1]
+                temp += 1
+            else:
+                break
+        answer = max(temp, answer)
+        if answer == len(dungeons):
+            return answer
+            
     return answer
