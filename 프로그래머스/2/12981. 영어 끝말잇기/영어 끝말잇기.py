24b98e2da2cc42a0435713e30e59f67a19d2536e
KelseyKwon <nykwon7777@gmail.com>
2025-09-06 16:03:22 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,30 +1,38 @@
-import math
-def solution(n, words):
-    answer = []
-    """
-    2<= n <= 10
-    n<= 배열의 길이 <= 100
-    2<= 단어의 길이 <= 50
-    [번호, 차례 형태로 return] 하기
-    
-    1) 이미 존재 하거나 -> set으로 만들고 전과 길이가 같으면 False하거나 ... 
-    2) 끝이 맞지 않으면 -> 이전[-1] == 지금[0]이 맞는지.
-    
-    차례 = (i // n) + 1, 순서 : (i % n) + 1
-    """
-    word_list = set()
-    word_list.add(words[0])
-    flag = words[0]
-    
-    for i in range(1, len(words)):
-        if words[i] in word_list or flag[-1] != words[i][0]:
-                   return [(i % n) + 1, (i // n) + 1]
-                   break
-        else: 
-            word_list.add(words[i])
-            flag = words[i]
-    
-    return [0, 0]
-                   
+"""
+1) 이전에 등장했던 단어 / 2) 한 글자인 단어
+들은 인정되지 않음
 
-    return answer
+사람수는 10명 이하
+단어의 길이는 2이상 50 이하
+번호, 차례 형태로 return하기 / 탈락자가 안 생기면 [0, 0]을 return하기
+
+task1 : 이미 등장했던 단어는? -> dict으로 처리
+task2 : 앞의 끝 문자로 끝나는지는? -> 일단 앞의 단어를 저장하고 -> 그 다음에 [-1] = [0]인지 확인
+task3 : result는 어떻게 반환해? -> 현재 index를 
+0, 1, 2 -> [1, 1] [2, 1] [3, 1]
+3, 4, 5 -> [1, 2] [2, 2] [3, 2]
+6, 7, 8 -> [1, 3] [2, 3] [3, 3] index % 3 + 1 / ceil(index + 1 / 3)
+
+"""
+from collections import defaultdict
+import math
+
+def solution(n, words):
+    m = len(words)
+    count = defaultdict(int)
+    prev_word = words[0]
+    count[prev_word] += 1
+    
+    for i in range(1, m):
+        # 이미 등장했는지 확인
+        if (count[words[i]] != 0):
+            return [i % n + 1, math.ceil((i + 1) / n)]
+        
+        # 앞의 단어의 끝과 안 맞는지 확인
+        if (prev_word[-1] != words[i][0]):
+            return [i % n + 1, math.ceil((i + 1) / n)]
+        
+        prev_word = words[i]
+        count[prev_word] += 1
+
+    return [0, 0]
