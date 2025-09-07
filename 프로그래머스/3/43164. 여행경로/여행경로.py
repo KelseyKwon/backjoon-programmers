a8ef006c2bdc9ae5c3e2f99664df3590b7444413
KelseyKwon <nykwon7777@gmail.com>
2025-09-07 14:39:05 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,28 +1,52 @@
+"""
+공항 수는 3<= <=10000
+[a, b] means a -> b exists
+
+linked_list 사용해서 dfs!
+
+ICN -> JFK
+HND -> IAD
+JFK -> HND
+
+--> ICN -> JFK -> HND -> IAD
+
+ICN -> SFO
+ICN -> ATL
+SFO -> ATL
+ATL -> ICN
+ATL -> SFO
+
+ICN : SFO, ATL -> 정렬해 ICN -> ATL -> ICN -> SFO -> ATL -> SFO
+
+이차원 배열을 선언해서 -> 인접리스트형식으로 만들기.
+
+1) 인접리스트 (이차원배열)
+2) dfs -> ICN부터 시작 -> 만약에 티켓을 사용하지 않았고, 각 배열의 첫번쨰 도시부터 돌아
+3) 그리고 없으면 빠져나와서 다음 큐부터
+"""
 from collections import defaultdict
 
 def solution(tickets):
-    answer = []
-    # 1. 초기화
-    linked_list = defaultdict(list)
-    cities = set()
+    graph = defaultdict(list)
     
-    for rows in tickets:
-        linked_list[rows[0]].append(rows[1])
+    for depart, destination in tickets:
+        graph[depart].append(destination)
     
-    # 2. 알파벳으로 정렬
-    for k in linked_list.keys():
-        linked_list[k].sort(reverse = True)
+    for k in graph:
+        graph[k].sort(reverse=True)
         
+    route = []
     stack = ["ICN"]
+    
+    # stack에 값이 있는 동안
     while stack:
-        s = stack[-1]
-        if not linked_list[s]:
-            answer.append(stack.pop())
+        cur = stack[-1]
+        # 아직 방문하지 않았다면
+        if graph[cur]:
+            stack.append(graph[cur].pop())
+        # 방문 했으면
         else:
-            stack.append(linked_list[s].pop())
-    return answer[::-1]
+            route.append(stack.pop())
+        
     
-    
-    
-    
-    return answer
+    return route[::-1]
