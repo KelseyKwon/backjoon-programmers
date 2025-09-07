8298a2b1342df68184889ff7cf8f31276dc80737
KelseyKwon <nykwon7777@gmail.com>
2025-09-07 14:05:20 +0900

    [level 2] Title: 전화번호 목록, Time: 119.29 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,26 +1,26 @@
 """
-def all_shortest(strings):
-  min_len = min(len(s) for s in strings)
-  return [s for s in strings if len(s) == min_len]
+한 번호가 다른 번호의 접두어인지 확인하는법
 
-def solution(phone_book):
-    answer = True
+일단 배열을 순서대로 정렬하고
 
-    candidate = set(all_shortest(phone_book))
-    N = min(len(s) for s in phone_book)
+119 1195524421
 
-    for i in phone_book:
-      if len(i) > N and i[0:N] in candidate:
-        return True
-    else:
-      return False
+뒤에 있는 원소가 직전에 있는 원소로 시작하면 false 반환
 """
 
 def solution(phone_book):
-  phone_book.sort()
-
-  for i in range(len(phone_book) - 1):
-    if phone_book[i+1].startswith(phone_book[i]):
-      return False
-
-  return True
+    answer = True
+    
+    phone_book.sort()
+    prefix = phone_book[0].strip()
+    
+    if (len(phone_book) == 1):
+        return True
+    
+    for i in range(1, len(phone_book)):
+        if (phone_book[i].strip().startswith(prefix)):
+            return False
+        else:
+            prefix = phone_book[i].strip()
+    
+    return True
