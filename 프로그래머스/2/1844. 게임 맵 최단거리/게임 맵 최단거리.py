b91d2b5996bce56d0447dceb0d9bbf68e4daca95
KelseyKwon <nykwon7777@gmail.com>
2025-09-09 17:17:31 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.61 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,38 +1,36 @@
 """
-    최단거리 -> bfs -> queue -> 2차원이니까 dx, dy 정의 , 배열에 count 더하기
-    """
+1 : 갈 수 있는 길
+0 : 갈 수 없는 길
+
+
+bfs -> 상, 하, 좌, 우 확인해서 1이고, 범위 안에 있고, 방문하지 않았으면 -> go
+"""
 from collections import deque
 
 def solution(maps):
+    N, M = len(maps), len(maps[0])
     dx = [0, 1, 0, -1]
     dy = [1, 0, -1, 0]
+    visited = [[False] * M for _ in range(N)]
+    
     def inRange(x, y):
-        return 0<=x<n and 0<=y<m
+        return 0 <= x < N and 0 <= y < M
     
-    def bfs(x, y):
-        q = deque()
-        q.append((x, y, 1))
-        visited[x][y] = True
+    def bfs(start_x, start_y):
+        if maps[start_x][start_y] == 0:
+            return -1
+        queue = deque([(start_x, start_y, 1)])   #튜플 하나를 원소로
+        visited[start_x][start_y] = True
         
-        while q:
-            cur_x, cur_y, dist = q.popleft()
-            if cur_x == n-1 and cur_y == m-1:
-                return dist
-            
+        while queue:
+            x, y, cost = queue.popleft()
+            if x == N - 1 and y == M - 1:
+                return cost
             for i in range(4):
-                nx = cur_x + dx[i]
-                ny = cur_y + dy[i]
-                
-                if inRange(nx, ny) and not visited[nx][ny] and maps[nx][ny] == 1:
+                nx, ny = x + dx[i], y + dy[i]
+                if inRange(nx, ny) and maps[nx][ny] == 1 and not visited[nx][ny]:
                     visited[nx][ny] = True
-                    q.append((nx, ny, dist+1))
-                    
+                    queue.append((nx, ny, cost + 1))  #튜플로 넣기
         return -1
-                    
-        
-    n = len(maps)
-    m = len(maps[0])
-    
-    visited = [[False] * m for _ in range(n)]
+
     return bfs(0, 0)
-    
