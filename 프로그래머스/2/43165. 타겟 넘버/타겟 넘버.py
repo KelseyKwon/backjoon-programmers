8769a9ad883da6be60c8e293342a6d25624aafe4
KelseyKwon <nykwon7777@gmail.com>
2025-09-17 18:29:33 +0900

    [level 2] Title: 타겟 넘버, Time: 409.77 ms, Memory: 9.02 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,32 +1,12 @@
-# answer_count = 0
+"""
 
-# def dfs(numbers, result, cur_index, visited, target):
-#   if (cur_index == 4):
-#     return result
-
-#   for i in range(1, len(numbers)+1):
-#     if not visited[i]:
-#       visited[i] = True
-#       first = dfs(numbers, result+numbers[cur_index], cur_index+1, visited, target)
-#       if (first == target):
-#         answer_count += 1
-#       second = dfs(numbers, result-numbers[cur_index], cur_index+1, visited, target)
-#       if (second == target):
-#         answer_count += 1
-
-
-
-# def solution(numbers, target):
-#     visited = [[False] * (len(numbers)+1)]
-#     dfs(numbers, 0, 0, visited,)
-#     return answer_count
+"""
 
 def solution(numbers, target):
-  def dfs(index, total):
-    if index == len(numbers):
-      return 1 if total == target else 0
-    
-    plus = dfs(index + 1, total + numbers[index])
-    minus = dfs(index + 1, total - numbers[index])
-    return plus + minus
-  return dfs(0, 0)
+    def dfs(cur, index):
+        if (index == len(numbers)):
+            return 1 if cur == target else 0
+        plus = dfs(cur + numbers[index], index+1)
+        minus = dfs(cur - numbers[index], index+1)
+        return plus + minus
+    return dfs(0, 0)
