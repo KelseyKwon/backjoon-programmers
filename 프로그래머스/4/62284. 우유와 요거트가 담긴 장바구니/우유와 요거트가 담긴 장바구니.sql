fe5946c7c96a8c9f91e9e9d83ce89537d5b33228
KelseyKwon <nykwon7777@gmail.com>
2025-09-17 19:35:12 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,12 +1,12 @@
 -- 코드를 입력하세요
--- Name에 Milk 와 YOgurt가 둘다 있는지 알아보기
--- 둘다 동시에 구입한 장바구니 아이디 조회하는 SQL
--- CART_ID asc
-(select CART_ID
+-- Milk & Yogurt가 모두 있는 장바구니 알아보기
+-- 
+(SELECT CART_ID
 from CART_PRODUCTS
-where NAME = 'Milk')
-intersect
-(select CART_ID
-from cART_PRODUCTS
 where NAME = 'Yogurt')
-order by CART_ID asc
+intersect
+(
+select CART_ID
+    from CART_PRODUCTS
+    where NAME = 'Milk'
+)
