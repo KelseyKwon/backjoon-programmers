95f54ba2301f072c04497445778f8138fdc25074
KelseyKwon <nykwon7777@gmail.com>
2025-09-19 23:06:46 +0900

    [level 3] Title: 기지국 설치, Time: 5.01 ms, Memory: 9.66 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -1,43 +1,57 @@
-# import math
+"""
+installed 
+1. 일단 stations의 숫자마다 좌, 우 w만큼 installed에 True
+2. 그리고 각 N만큼 -> 만약에 x installed -> temp += 현재 index
+그리고 count = 3이거나, installed = True이면 -> temp / w , 그리고 temp = 0으로 초기화
+"""
 
 # def solution(n, stations, w):
 #     answer = 0
-#     def inRange(index):
-#       return index >= 1 and index <= n
 
-#     # 전파가 전달되는 기지국
-#     visited = [False] * n
+#     installed = [False for _ in range(n+1)]
+    
 #     for i in stations:
-#       for j in range(i-w, i+w+1):
-#         if inRange(j):
-#           visited[j-1] = True
+#         for j in range(i-w, i+w+1):
+#             if (j >= 1 and j <= n):
+#                 installed[j] = True
     
 #     count = 0
-#     for i in range(len(visited)):
-#       if (visited[i]):
-#         answer += math.ceil(count / (2*w+1))
-#         count = 0
-#       else:
-#         count += 1
-#         print(i, count)
-#     answer += math.ceil(count / (2*w + 1))
+#     for i in range(1, n+1):
+#         if (count == 2*w+1 or installed[i] == True):
+#             for j in range(i-w, i+w+1):
+#                 if (j >= 1 and j <= n):
+#                     installed[j] = True
+#                     print("색칠함", j)
+#                     count = 0
+#             answer += 1
+#         else:
+#             print("아님" , i)
+#             count += 1
 
 #     return answer
 
+"""
+그럼 일단 빈칸을 찾아 -> 그리고 해당 빈칸의 길이를 2*w+1로 나누거의 ceil만큼 더해 answer에
+"""
 import math
 
 def solution(n, stations, w):
     answer = 0
-    ranges = 2*w + 1
     
-    cur = 1
+    length = 2*w+1
+    prev = 1
+    
     for i in stations:
-      if i - w > cur:
-        gap = (i-w) - cur
-        answer += math.ceil(gap / ranges)
-      cur = i + w + 1
-    # 남은 구간
-    if cur <= n:
-      gap = n - cur + 1
-      answer += math.ceil(gap / ranges)
+        L = max(1, i-w)
+        R = min(n, i+w)
+        
+        gap = L - prev
+        if gap > 0:
+            answer += math.ceil(gap / length)
+        
+        prev = R+1
+    
+    if prev <= n:
+        answer += math.ceil((n-prev+1) / length)
+    
     return answer
