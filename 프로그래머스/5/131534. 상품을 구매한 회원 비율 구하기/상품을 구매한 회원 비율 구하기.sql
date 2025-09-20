31ce32a2531897e7f06b93f34c65c204b94813ec
KelseyKwon <nykwon7777@gmail.com>
2025-09-20 18:41:10 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,26 +1,25 @@
 -- 코드를 입력하세요
--- JOINED = 2021
--- ONLINE_SALE의 USER_ID가 not null
--- YEAR, MONTH 별로 출력
--- round 소수점 두번째 자리
--- YEAR asc, MONTH asc
--- group by YEAR(SALES_DATE)
-WITH joined_2021 AS (
-    SELECT USER_ID
-    FROM USER_INFO
-    WHERE YEAR(JOINED) = 2021
+-- GENDER : NULL or 0 (MALE), 1 (FEMALE)
+-- (날짜, 회원 ID, 상품 ID)는 하나의 판매 데이터만
+-- JOIN = '2021', ONLINE_SALE에 있고 / JOIN = '2021'을 group by 년, SALES_DATE의 month로 출력하기.
+-- 두번째 자리에서 반올림
+-- YEAR(SALES_DATE) asc, MONTH(SALES_DATE) desc
+-- 
+with joined_2021 as (
+    select DISTINCT USER_ID
+    from USER_INFO
+    where YEAR(JOINED) = '2021'
+), total_2021 as (
+select count(*) as total_user
+from joined_2021
 )
-
-SELECT
-    YEAR(b.SALES_DATE) AS YEAR,
-    MONTH(b.SALES_DATE) AS MONTH,
-    COUNT(DISTINCT b.USER_ID) AS PURCHASED_USERS,
-    ROUND(
-        COUNT(DISTINCT b.USER_ID) * 1.0 / (SELECT COUNT(*) FROM joined_2021),
-        1
-    ) AS PUCHASED_RATIO
-FROM ONLINE_SALE b
-JOIN joined_2021 j
-  ON b.USER_ID = j.USER_ID
-GROUP BY YEAR(b.SALES_DATE), MONTH(b.SALES_DATE)
-ORDER BY YEAR(b.SALES_DATE), MONTH(b.SALES_DATE);
+SELECT 
+YEAR(b.SALES_DATE) as YEAR, 
+MONTH(b.SALES_DATE) as MONTH, 
+count(DISTINCT b.USER_ID) as PURCHASED_USERS, 
+ROUND(count(DISTINCT b.USER_ID) * 1.0 / t.total_user, 1) as PURCHASED_RATIO
+from joined_2021 as a
+join ONLINE_SALE as b on a.USER_ID = b.USER_ID
+cross join total_2021 as t
+group by YEAR(b.SALES_DATE), MONTH(b.SALES_DATE)
+order by YEAR(b.SALES_DATE) asc, MONTH(b.SALES_DATE) asc
