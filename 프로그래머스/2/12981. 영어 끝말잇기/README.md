74a84518938b5130aac2cef29f72a6ed1174a3e7
KelseyKwon <nykwon7777@gmail.com>
2025-09-21 14:23:47 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
 
 ### 성능 요약
 
-메모리: 9.23 MB, 시간: 0.08 ms
+메모리: 9.23 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 06일 16:03:20
+2025년 09월 21일 14:23:44
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,38 +1,18 @@
 """
-1) 이전에 등장했던 단어 / 2) 한 글자인 단어
-들은 인정되지 않음
+이전에 등장했던 단어 x & 한 글자인 단어는 인정되지 x
 
-사람수는 10명 이하
-단어의 길이는 2이상 50 이하
-번호, 차례 형태로 return하기 / 탈락자가 안 생기면 [0, 0]을 return하기
-
-task1 : 이미 등장했던 단어는? -> dict으로 처리
-task2 : 앞의 끝 문자로 끝나는지는? -> 일단 앞의 단어를 저장하고 -> 그 다음에 [-1] = [0]인지 확인
-task3 : result는 어떻게 반환해? -> 현재 index를 
-0, 1, 2 -> [1, 1] [2, 1] [3, 1]
-3, 4, 5 -> [1, 2] [2, 2] [3, 2]
-6, 7, 8 -> [1, 3] [2, 3] [3, 3] index % 3 + 1 / ceil(index + 1 / 3)
 
 """
-from collections import defaultdict
-import math
 
 def solution(n, words):
+    spoken_word = {words[0]}
     m = len(words)
-    count = defaultdict(int)
-    prev_word = words[0]
-    count[prev_word] += 1
     
-    for i in range(1, m):
-        # 이미 등장했는지 확인
-        if (count[words[i]] != 0):
-            return [i % n + 1, math.ceil((i + 1) / n)]
-        
-        # 앞의 단어의 끝과 안 맞는지 확인
-        if (prev_word[-1] != words[i][0]):
-            return [i % n + 1, math.ceil((i + 1) / n)]
-        
-        prev_word = words[i]
-        count[prev_word] += 1
-
+    for i in range(1, len(words)):
+        prev = words[i-1]
+        cur = words[i]
+        # 규칙 위반: 끝 글자 불일치 or 중복 단어
+        if prev[-1] != cur[0] or cur in spoken_word:
+            return [i % n + 1, i // n + 1]
+        spoken_word.add(cur)               # 검증 통과 후에 추가
     return [0, 0]
