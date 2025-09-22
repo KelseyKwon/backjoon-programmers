bba1c6f49e82b8bbb7bf58fbc722512bf2be51ef
KelseyKwon <nykwon7777@gmail.com>
2025-09-22 16:52:02 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,6 +1,8 @@
 -- 코드를 입력하세요
--- 들어올때 SEX_UPON_INTAKE가 INTACT, 나갈떄 SEX_UPON_OUTCOME이 neutered or spayed인거.
+-- ANIMAL_INS에서는 SEX_UPON_INTAKE이 Intact였지만, ANIMAL_OUTS에서는 SEX_UPON_OUTCOME이 Spayed or Neutered인것. -> 
+-- ANIMAL_ID, ANIMAL_TYPE, NAME, 
 SELECT a.ANIMAL_ID, a.ANIMAL_TYPE, a.NAME
-from ANIMAL_INS as a join ANIMAL_OUTS as b
-on a.ANIMAL_ID = b.ANIMAL_ID
-where a.SEX_UPON_INTAKE like 'Intact%' and (b.SEX_UPON_OUTCOME like 'Neutered%' or b.SEX_UPON_OUTCOME like 'Spayed%')
+from ANIMAL_INS as a
+join ANIMAL_OUTS as b on a.ANIMAL_ID = b.ANIMAL_ID
+where a.SEX_UPON_INTAKE like "Intact%" and (b.SEX_UPON_OUTCOME like "Spayed%" or b.SEX_UPON_OUTCOME like "Neutered%")
+ORDER BY a.ANIMAL_ID;
