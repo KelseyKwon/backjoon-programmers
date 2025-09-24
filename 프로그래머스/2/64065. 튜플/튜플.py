8a04e4bed41c38f2dba96767147308424c899bd2
KelseyKwon <nykwon7777@gmail.com>
2025-09-24 15:20:02 +0900

    [level 2] Title: 튜플, Time: 49.53 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,11 +1,23 @@
+"""
+튜플에는 중복 ok, 순서가 다르면 다른 튜플, 원소 개수는 유한하다.
+집합은 순서가 바뀌어도 같은 집합.
+집합을 길이를 기준으로 정렬해 -> 그리고 
+"""
+
 def solution(s):
     answer = []
-    s_list = s[2:-2].split('},{')
-    w_list = [w.split(',') for w in s_list]
-
+    temp = set()
+    
+    s_list = s[2:-2].split("},{")
+    w_list = [w.split(",") for w in s_list]
     w_list.sort(key = len)
-    for a in w_list:
-      for w in a:
-        if int(w) not in answer:
-          answer.append(int(w))
+
+    for now in w_list:
+        for now_num in now:
+            cur_num = int(now_num)
+            if cur_num not in temp:
+                answer.append(cur_num)
+                temp.add(cur_num)
+                
+    
     return answer
