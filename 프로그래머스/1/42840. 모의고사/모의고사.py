41140f686158bcb782f18d5ef6eccb0b45439d84
KelseyKwon <nykwon7777@gmail.com>
2025-09-25 20:41:50 +0900

    [level 1] Title: 모의고사, Time: 3.64 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,23 +1,26 @@
+"""
+일단 학생별로 맞은 학생을 담는 dictionary 선언
+"""
+
 def solution(answers):
-    # 수포자들의 찍기 패턴
-    patterns = [
-        [1, 2, 3, 4, 5],
-        [2, 1, 2, 3, 2, 4, 2, 5],
-        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    ]
-
-    # 각 수포자의 점수
-    scores = [0, 0, 0]
-
-    for i, answer in enumerate(answers):
-        for idx, pattern in enumerate(patterns):
-            if answer == pattern[i % len(pattern)]:
-                scores[idx] += 1
-
-    # 최고 점수 계산
-    max_score = max(scores)
-
-    # 최고 점수자만 추출 (1-based index)
-    result = [i + 1 for i, score in enumerate(scores) if score == max_score]
-
-    return result
+    answer = []
+    
+    scores = {1: 0, 2: 0, 3: 0}
+    answer_1 = [1, 2, 3, 4, 5]
+    answer_2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    answer_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
+    for i in range(len(answers)):
+        if answers[i] == answer_1[i % len(answer_1)]:
+            scores[1] += 1
+        if answers[i] == answer_2[i % len(answer_2)]:
+            scores[2] += 1
+        if answers[i] == answer_3[i % len(answer_3)]:
+            scores[3] += 1
+    
+    max_score = max(scores.values())
+    for k in scores.keys():
+        if scores[k] == max_score:
+            answer.append(k)
+            
+    return answer
