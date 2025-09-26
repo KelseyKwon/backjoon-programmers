52ff92869dee5c092eaecd92767c8f5e84c5dc3d
KelseyKwon <nykwon7777@gmail.com>
2025-09-26 14:13:37 +0900

    [level 2] Title: 주식가격, Time: 25.49 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,20 +1,30 @@
-"""
-prices의 값은 10000이하 자연수, 길이는 100000이하.
-
-"""
-
 def solution(prices):
-    answer = []
     n = len(prices)
+    answer = [0] * n
     
-    # O(N^2)
-    for i in range(n):
-        cur = prices[i]
-        for j in range(i, n):
-            if (prices[j] < cur):
-                answer.append(j-i)
-                break
-        else:
-            answer.append(n-i-1)
-    
+    stack = [0]
+    for i in range(1, n):
+        while stack and prices[i] < prices[stack[-1]]:
+            j = stack.pop()
+            answer[j] = i-j
+        stack.append(i)
+        
+    # 스택에 남아있는 것들은 가격이 떨어지지 않는 것.
+    while stack:
+        j = stack.pop()
+        answer[j] = n - 1 - j
     return answer
+
+"""
+def solution(prices):
+    length = len(prices)
+    answer = [ i for i in range (length - 1, -1, -1)]
+    
+    stack = [0]
+    for i in range (1, length, 1):
+        while stack and prices[stack[-1]] > prices[i]:
+            j = stack.pop()
+            answer[j] = i - j
+        stack.append(i)
+    return answer
+"""
