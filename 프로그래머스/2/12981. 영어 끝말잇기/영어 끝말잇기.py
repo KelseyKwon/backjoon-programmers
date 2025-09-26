b05f35104861b96956184d14ad4cee0a2374ffd2
KelseyKwon <nykwon7777@gmail.com>
2025-09-26 15:38:25 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,18 +1,28 @@
 """
-이전에 등장했던 단어 x & 한 글자인 단어는 인정되지 x
+말한거 또 말하기 x, 규칙 지켜야 함. 
+1. 말한거 또 말하기 x : not in set
+2. 규칙 지켜야함 : prev저장하고, prev[-1] = cur[0]이면 ok 
 
+1, 2번 중 하나라도 안 지켜지면 return 
+순서 : index % n + 1
+번호 : index // n + 1 이렇게 반환.
 
+아니면 -> [0, 0] 반환
 """
 
 def solution(n, words):
-    spoken_word = {words[0]}
-    m = len(words)
+    answer = []
+
+    prev = words[0]
+    said_word = set()
+    said_word.add(prev)
     
     for i in range(1, len(words)):
-        prev = words[i-1]
+        # 말한거 또 말하기 x
         cur = words[i]
-        # 규칙 위반: 끝 글자 불일치 or 중복 단어
-        if prev[-1] != cur[0] or cur in spoken_word:
+        if cur in said_word or prev[-1] != cur[0]:
             return [i % n + 1, i // n + 1]
-        spoken_word.add(cur)               # 검증 통과 후에 추가
+        else:
+            said_word.add(cur)
+            prev = cur
     return [0, 0]
