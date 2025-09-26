59dec9b9a882569b931269861cb84597b1853ca5
KelseyKwon <nykwon7777@gmail.com>
2025-09-26 17:22:39 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,13 +1,12 @@
 -- 코드를 작성해주세요
--- 10cm이하면 NULL. 
--- 종류별로 가장 큰 물고기의 ID, FISH_NAME, LENGTH
-SELECT a.ID, b.FISH_NAME, a.LENGTH
-FROM FISH_INFO a
-JOIN FISH_NAME_INFO b ON a.FISH_TYPE = b.FISH_TYPE
-WHERE a.LENGTH > 10
-  AND a.LENGTH = (
-    SELECT MAX(LENGTH)
-    FROM FISH_INFO
-    WHERE FISH_TYPE = a.FISH_TYPE AND LENGTH > 10
-  )
-ORDER BY a.ID;
+-- 길이가 10 이하면 => LENGTH = NULL IFNULL(LENGTH, 10)
+-- group by FISH_TYPE -> LENGTH가 가장 큰 물고기의 a.ID, b.FISH_NAME, a.LENGTH
+with deleted_null as (
+    select ID, FISH_TYPE, IFNULL(LENGTH, 10) as LENGTH, TIME
+from FISH_INFO as a
+)
+select a.ID, b.FISH_NAME, a.LENGTH
+from deleted_null as a
+join FISH_NAME_INFO as b on a.FISH_TYPE = b.FISH_TYPE
+where (a.FISH_TYPE, a.LENGTH) in (select FISH_TYPE, max(LENGTH) from deleted_null group by FISH_TYPE)
+order by a.ID asc
