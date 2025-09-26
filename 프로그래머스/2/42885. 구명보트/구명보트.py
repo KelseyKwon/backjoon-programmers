c1fe1ab41066d0a37fbf5f0292ee1a950065f4a9
KelseyKwon <nykwon7777@gmail.com>
2025-09-26 15:43:22 +0900

    [level 2] Title: 구명보트, Time: 9.77 ms, Memory: 9.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,19 +1,24 @@
 """
-최대 2명, 무게 제한 O
+최대 2명
 
+100kg -> 2번쨰 사람과 4번째 사람은 같이 탈 수 있다.
+
+[50, 50, 70, 80]
+일단 무거운 사람은 무조건 태우고 -> 가벼운 사람을 태웠을때 제한이 안넘어가면 태우고 -> 아니면 그대로 둠
+투포인터!
 """
 
 def solution(people, limit):
     answer = 0
     
     people.sort()
-    right = len(people) - 1
     left = 0
+    right = len(people) - 1
     
-    while (right >= left):
-        temp = people[right]
-        if (temp + people[left] <= limit):
+    while (left <= right):
+        if (people[left] + people[right] <= limit):
             left += 1
         right -= 1
         answer += 1
+    
     return answer
