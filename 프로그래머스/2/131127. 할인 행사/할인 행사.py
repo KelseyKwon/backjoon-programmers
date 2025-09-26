fc6e049b35bc882ae19567b3459ab88c0a08bd4a
KelseyKwon <nykwon7777@gmail.com>
2025-09-26 09:03:21 +0900

    [level 2] Title: 할인 행사, Time: 150.68 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,66 +1,24 @@
 """
-number의 원소의 합은 10
-각 단어의 길이는 12 이하.
-discount : 14개 
-
-discount의 인덱스 9 ~ len(discount) - 1 까지
-잘라 그리고 각 배열마다 각 number의 원소들을 1식 줄여
-
-그리고 모든 number의 원소가 0이 되면 해당 날짜를 반환해 (인덱스 + 1)
+제품 & 수량이 10일 연속ㅇ로 일치할 때.
+sliding window! 
 """
-
-"""
-def solution(want, number, discount):
-    answer = 0
-    
-    for i in range(9, len(discount)-1):
-        temp_discount = discount[i-9:i+1]
-        for k in temp_discount:
-            for j in range(len(want)):
-                if want[j] == k:
-                    number[j]-=1
-        # 모든 원소가 0인지 확인하는법
-        if (all(x == 0 for x in number)):
-            return i+1
-    
-    return 0
-"""
-
-"""
-슬라이딩 윈도우!
-Counter는 파이썬 collections모듈에 있는, 빈도수 세기 전용 딕셔너리
-"""
+from collections import defaultdict
 from collections import Counter
 
 def solution(want, number, discount):
-    need = dict(zip(want, number))
-    n = len(discount)
+    answer = 0
     
-    # number의 합이 10이니까 discount의 원소의 길이 10이 안되면 조건 미충족
-    if n < 10:
-        return 0
+    my_fruit = {}
     
-    #초기 10일 빈도
-    window_cnt = Counter(discount[:10])
+    for fruit, i in zip(want, number):
+        my_fruit[fruit] = i
     
-    def ok():
-        return all(window_cnt.get(item, 0) >= need[item] for item in need)
-    
-    answer = 1 if ok() else 0
-    
-    for i in range(10, n):
-        out_item = discount[i-10]
-        in_item = discount[i]
-        
-        # 윈도우에서 빠지는 항목
-        window_cnt[out_item] -= 1
-        if window_cnt[out_item] == 0:
-            del window_cnt[out_item]
-            
-        #윈도우에 새로 들어오는 항목
-        window_cnt[in_item] += 1
-        
-        if ok():
+    for i in range(len(discount) - 9):   
+        discount_fruit = Counter(discount[i:i+10])
+        # for k in my_fruit.keys():
+        #     discount_fruit[k] -= my_fruit[k]
+        # if (all(x == 0 for x in discount_fruit.values())):
+        #     return i-9
+        if discount_fruit == my_fruit:
             answer += 1
-            
     return answer
