7f518e26ed3886588173b9259aed9807ba75e952
KelseyKwon <nykwon7777@gmail.com>
2025-09-26 22:21:49 +0900

    [level 2] Title: 무인도 여행, Time: 19.84 ms, Memory: 9.5 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,52 +1,50 @@
 """
-1. 일단 maps를 1 -> 2차원 배열로
-2. bfs : 상, 하, 좌, 우를 탐색하고 -> queue.append(해당 칸의 좌표, 먹이량) 더해.
-3. 그리고 다 했으면 -> answer.append()
+queue 선언.
+
+각 지점에서, 'X'가 아니면 -> dfs(x, y)
+queue.append해 -> 그다음에 range(4)동안 nx해 -> 
+inRange() & not visited & maps != 'X':
+.append(x, y, answer)
 """
 from collections import deque
 
 def solution(maps):
     answer = []
-    n = len(maps)
-    m = len(maps[0])
-    
-    # #maps[0]의 length만큼, 각 maps
-    # map = [[maps[i][j] for j in range(m)] for i in range(n)]
-    # print(map)
+    n, m = len(maps), len(maps[0])
     visited = [[False] * m for _ in range(n)]
+    
     dx = [0, 1, 0, -1]
     dy = [1, 0, -1, 0]
     
     def inRange(x, y):
         return 0<=x<n and 0<=y<m
     
-    def bfs(x, y):
-        q = deque([(x, y)])
+    def dfs(x, y, food):
+        q = deque()
+        q.append((x, y))
         visited[x][y] = True
         total = int(maps[x][y])
         
         while q:
-            cur_x, cur_y= q.popleft()
-            
+            cur_x, cur_y = q.popleft()
             for i in range(4):
                 nx = cur_x + dx[i]
                 ny = cur_y + dy[i]
                 
-                if inRange(nx, ny) and not visited[nx][ny] and maps[nx][ny] != 'X':
-                    q.append((nx, ny))
-                    visited[nx][ny] = True
+                if (inRange(nx, ny) and not visited[nx][ny] and maps[nx][ny] != 'X'):
                     total += int(maps[nx][ny])
+                    visited[nx][ny] = True
+                    q.append((nx, ny))
         return total
-            
+        
     
     for i in range(n):
         for j in range(m):
             if not visited[i][j] and maps[i][j] != 'X':
-                answer.append(bfs(i, j))
+                answer.append(dfs(i, j, int(maps[i][j])))
     
-    # if (len(answer) == 0):
     if not answer:
-        # answer.append[-1]
         return [-1]
-    answer.sort()
-    return answer
+    else:
+        answer.sort()
+        return answer
