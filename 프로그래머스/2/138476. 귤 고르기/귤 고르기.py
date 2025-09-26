0fb8fcf71d362a9c1230848aa2031fe42f83261c
KelseyKwon <nykwon7777@gmail.com>
2025-09-26 16:20:57 +0900

    [level 2] Title: 귤 고르기, Time: 41.81 ms, Memory: 20.9 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,38 +1,27 @@
 """
-귤을 크기별로 분류 -> 서로 다른 종류의 수를 최소화하고 싶다.
+서로 다른 종류의 귤의 개수가 최소가 되어야 함.
 
+1 : 1
+2 : 2
+3 : 2
+4 : 1
+5 : 2
+
+values을 기준으로 정렬 (내림차순으로) -> k-= 
 """
-from collections import defaultdict
+from collections import Counter
 
 def solution(k, tangerine):
-    answer = 0
-    info = defaultdict(int)
+    count = 0
     
-    #dict 생성
-    for i in tangerine:
-        info[i] += 1
-    # 딕셔너리는 .sort()의 메서드를 갖고 있지 않다.
-    # info.sort(key= lambda x: info[x], reverse = True)
-    counts = sorted(info.values(), reverse = True)
+    info = Counter(tangerine)
+    sort_info = sorted(info.values(), reverse = True)
     
-    # for key in info.keys():
-    #     temp = info[key]
-    #     if k >= temp:
-    #         k -= temp
-    #         answer += 1
-    #         continue
-    #     elif k>0 and k< emp:
-    #         answer += 1
-    #         break
-    #     elif k == 0:
-    #         break
-    # 이렇게 세가지로 경우를 쪼개는 것보다 그냥 k > temp이면 무조건 1 더하고,
-    # k <= temp이면 1더하고 break하는게 더 나음.
-    for c in counts:
-        if k > c:
-            k -= c
-            answer += 1
+    for i in sort_info:
+        if (k - i) > 0:
+            k -= i
+            count += 1
         else:
-            answer += 1
-            break
-    return answer
+            return count + 1
+    
+    return count
