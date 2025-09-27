51f6bf5de099b3728764f026c0ab5b58356cf1ab
KelseyKwon <nykwon7777@gmail.com>
2025-09-27 12:03:58 +0900

    [level 2] Title: 주식가격, Time: 26.12 ms, Memory: 18.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,30 +1,16 @@
 def solution(prices):
-    n = len(prices)
-    answer = [0] * n
+    answer = [0] * len(prices)
+    stack = []
     
-    stack = [0]
-    for i in range(1, n):
-        while stack and prices[i] < prices[stack[-1]]:
-            j = stack.pop()
-            answer[j] = i-j
-        stack.append(i)
-        
-    # 스택에 남아있는 것들은 가격이 떨어지지 않는 것.
+    for index, p in enumerate(prices):
+        while stack and prices[stack[-1]] > p:
+            i = stack.pop()
+            answer[i] = index - i
+        stack.append(index)
+    
+    maxx = len(prices) - 1
     while stack:
-        j = stack.pop()
-        answer[j] = n - 1 - j
-    return answer
-
-"""
-def solution(prices):
-    length = len(prices)
-    answer = [ i for i in range (length - 1, -1, -1)]
+        cur = stack.pop()
+        answer[cur] = maxx - cur
     
-    stack = [0]
-    for i in range (1, length, 1):
-        while stack and prices[stack[-1]] > prices[i]:
-            j = stack.pop()
-            answer[j] = i - j
-        stack.append(i)
     return answer
-"""
