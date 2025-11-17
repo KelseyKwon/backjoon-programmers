8637c2dcd73e527dece75e91a9d3e21b12330151
KelseyKwon <nykwon7777@gmail.com>
2025-11-18 08:31:47 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.36 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,25 +1,19 @@
 """
-각 배열의 값끼리 더해서 가장 최소가 되는 값을 내놓기.
-그러면 각 배열을 먼저 정렬해  -> 
-처음배열은 오름차순, 뒤의 배열은 내림차순으로
-[1, 2, 4] [5, 4, 4] -> 5 + 8 + 16 -> 29
+가장 작은 숫자 -> 가장 큰 숫자랑 곱해
+[1, 2, 4] [5, 4, 4] 
 [1, 2] [4, 3] -> 4 + 6
+
 
 """
 
 def solution(A,B):
     answer = 0
-    
-    n = len(A)
 
     A.sort()
     B.sort(reverse = True)
     
-    print(A)
-    print(B)
+    for a, b in zip(A, B):
+        answer += a*b
     
-    for i in range(n):
-        answer += (A[i] * B[i])
-        print(answer)
 
     return answer
