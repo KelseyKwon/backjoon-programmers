f777ba9a09982656a64739222fddd2a518352791
KelseyKwon <nykwon7777@gmail.com>
2026-02-06 11:11:35 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,46 +1,38 @@
 """
-1, 2, 3번 서로 돌아가며 -> modulo 연산!
-앞사람이 안말한거 / 이전에 안등장한거 / 한글자가 아닌거
+이전에 말한 단어 사용 x
+한글자인 단어 인정 x
+마지막 사람 -> 1번부터 다시 시작...
 
-안말한거? -> set ! in set인지
-한글자가 아닌거? -> length
+0, 1, 2, 3, 4, 5
 
-번호
-0 -> 1 i % n + 1
-1 -> 2
-2 -> 3
-3 -> 1
+index % n -> 0, 1, 2
 
-8 -> 3 
+set에 추가하고, 
+1. 그 직전 배열의 길이랑 같거나
+2. 끝에가 다르거나,
 
-차례
-0 // 3 + 1
-3 // 3 + 1
-4 // 3 + 1
-5 // 3 + 1
+-> return
+0 // 3 -> 0
+번호 : i % n + 1, i // n + 1
+
 """
 
 def solution(n, words):
-    answer = []
-
-    m = len(words)
-    prev = words[0]
-    said_word = set()
-    said_word.add(prev)
+    answer = [0, 0]
     
-    for i in range(1, m):
-        cur = words[i]
-        person, turn = (i % n) + 1 , (i // n) + 1
+    prev_word = words[0]
+    no_duplicate_word = set([words[0]])
+    for i in range(1, len(words)):
+        cur_num = i % n + 1
+        cur_turn = i // n + 1
         
-        if cur in said_word:
-            print(person, turn)
-            return [person, turn]
-        
-        if prev[-1] != cur[0]:
-            return [person, turn]
-        
-        said_word.add(cur)
-        prev = cur
-        
+        # 중복되면 리턴
+        if words[i] in no_duplicate_word:
+                return [cur_num, cur_turn]
+        if (prev_word[-1] != words[i][0]):
+                return [cur_num, cur_turn]
+            
+        no_duplicate_word.add(words[i])
+        prev_word = words[i]
     
-    return [0, 0]
+    return answer
