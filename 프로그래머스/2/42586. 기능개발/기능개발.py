69e09620a28fba51a3302994765551ad6d3b25dd
KelseyKwon <nykwon7777@gmail.com>
2026-02-09 12:48:22 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,27 +1,29 @@
-import math
+"""
+뒤에 있는 기능이 앞에 있는 기능보다 먼저 개발 가능
 
+100 - speeds[i] -> 
+remain_days = [7, 3, 9] -> 7 > 3 -> 같이... 3 < 9 뒤에...
+remain_days = [5, 10, 1, 1, 20, 1] -> 1, 3, 2
+"""
+import math
 def solution(progresses, speeds):
     answer = []
+    nums = len(progresses)
+    remain_days = []
     
-    # ceil((100 - 93) / 1)
-    # 7, 3, 9
-    # while 더 작을때까지 ++
-    
-    # 5, 10, 1, 1, 20, 1
-    days = []
-    
-    for i in range(len(progresses)):
-        # [7, 3, 9]
-        days.append(math.ceil((100 - progresses[i]) / speeds[i]))
+    for i in range(nums):
+        remain_day = math.ceil((100 - progresses[i]) / speeds[i])
+        remain_days.append(remain_day)
         
-    flag = days[0]
+    prev_day = remain_days[0]
     count = 1
-    for i in range(1, len(days)):
-        if flag < days[i]:
-            flag = days[i]
+    for i in range(1, nums):
+        print(remain_days[i])
+        if (prev_day >= remain_days[i]):
+            count += 1
+        else:
+            prev_day = remain_days[i]
             answer.append(count)
             count = 1
-        else:
-            count += 1
     answer.append(count)
     return answer
