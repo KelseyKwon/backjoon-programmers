52aacb2819e10bfb6a7f7da33d90e0ccdcc7437d
KelseyKwon <nykwon7777@gmail.com>
2026-02-10 09:03:27 +0900

    [level 2] Title: 짝지어 제거하기, Time: 89.63 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,23 +1,22 @@
 """
-문자를 stack에 쌓아 -> 그리고 stack이고 ! stack의 top 이 현재랑 같으면 -> pop해. 
+baabaa -> bbaa -> aa -> x
+0
+
+
 """
 
 def solution(s):
     answer = -1
     
-    # b -> a -> a
-    # stack = list()
     stack = []
     
     for a in s:
-        # if stack and stack.top() == a:
         if stack and stack[-1] == a:
             stack.pop()
         else:
-            # stack.push(a)
             stack.append(a)
-    
-    if stack:
+
+    if (stack):
         return 0
     else:
         return 1
