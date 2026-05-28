62c66825c370ae98118ccf39464daa790a307584
권나연 <nykwon7777@gmail.com>
2026-05-28 12:05:20 +0900

    [level 2] Title: 구명보트, Time: 7.86 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,24 +1,20 @@
 """
-최대 2명
+50, 70, 80
+50, 50, 70, 80
 
-100kg -> 2번쨰 사람과 4번째 사람은 같이 탈 수 있다.
+일단 무거운 사람 태워 -> 만약에 첫번째 태울 수 있으면 -> okey!
 
-[50, 50, 70, 80]
-일단 무거운 사람은 무조건 태우고 -> 가벼운 사람을 태웠을때 제한이 안넘어가면 태우고 -> 아니면 그대로 둠
-투포인터!
 """
-
+from collections import deque
 def solution(people, limit):
-    answer = 0
-    
     people.sort()
-    left = 0
-    right = len(people) - 1
+    q = deque(people)
+    count = 0
     
-    while (left <= right):
-        if (people[left] + people[right] <= limit):
-            left += 1
-        right -= 1
-        answer += 1
-    
-    return answer
+    while q:
+        right = q.pop()
+        if q and (right + q[0] <= limit):
+            q.popleft()
+        count += 1
+    return count
+        
