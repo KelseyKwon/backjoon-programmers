774e7b5e6bb2653986a6b6f66004fea7532f9cfd
권나연 <nykwon7777@gmail.com>
2026-05-28 17:27:08 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,31 +1,26 @@
 """
-항상 "ICN" 공항에서 출발.
-방문하는 공항 경로를 배열에 담아 return
-
-항공권을 모두 사용해야 함.
-2개 이상 -> 알파벳 순서가 앞서는 것 (알파벳 순서대로 정렬)
+항상 ICN공항에서 출발.
+주어진 항공권을 모두 사용해야 함.
+알파벳 순서대로 정렬하기
 """
 from collections import defaultdict
 
 def solution(tickets):
-    answer = []
-    
-    info = defaultdict(list)
-    
+    graph = defaultdict(list)
     for depart, dest in tickets:
-        info[depart].append(dest)
-    # 알파벳 순서대로 정렬
-    for depart in info:
-        info[depart].sort(reverse = True)
-    
+        graph[depart].append(dest)
+        
+    for key in graph:
+        graph[key].sort(reverse = True)
+        
     stack = ["ICN"]
     route = []
+    
     while stack:
         cur = stack[-1]
-        if info[cur]:
-            stack.append(info[cur].pop())
+        if (graph[cur]):
+            stack.append(graph[cur].pop())
         else:
             route.append(stack.pop())
-            
     
     return route[::-1]
