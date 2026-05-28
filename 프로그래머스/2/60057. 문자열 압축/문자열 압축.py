47d7896a3ae19c4f17c573629c03f6d4540fa2e4
권나연 <nykwon7777@gmail.com>
2026-05-28 14:31:13 +0900

    [level 2] Title: 문자열 압축, Time: 0.94 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,34 +1,29 @@
-# 단어 앞에서부터 -> 1부터 2/len(s) + 1까지
-# stack에 쌓아. 그리고 stack[-1]이랑 같을때까지 반복해
-# 
-import sys
+"""
+s의 반까지만 하기
+그리고 최소 길이를 갱신
+1부터 s의 반까지
+"""
 
 def solution(s):
-    answer = sys.maxsize
-    
-    if (len(s)) == 1:
-        return 1
-    
-    
+    if (len(s) == 1): return 1
+    answer = len(s)
     for i in range(1, len(s) // 2 + 1):
         prev = s[:i]
+        _ans = ""
         count = 1
-        candidate = []
-        for step in range(i, len(s), i):
-            cur = s[step:step+i]
-            if cur == prev:
+        
+        for j in range(i, len(s), i):
+            cur = s[j:j+i]
+            if prev == cur:
                 count += 1
             else:
-                if count > 1:
-                    candidate.append(str(count))
-                candidate.append(prev)
+                # if count != 1:
+                #     _ans += cur
+                # else:
+                #     _ans += str(count) + cur
+                _ans += (str(count) if count > 1 else "") + prev
                 prev = cur
                 count = 1
-        if count > 1:
-            candidate.append(str(count))
-        candidate.append(prev)
-            
-        cur_answer = "".join(candidate)
-        answer = min(answer, len(cur_answer))
-        
+        _ans += (str(count) if count > 1 else "") + prev
+        answer = min(answer, len(_ans))
     return answer
