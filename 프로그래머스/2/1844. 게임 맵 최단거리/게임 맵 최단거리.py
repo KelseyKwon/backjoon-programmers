c828737204b5cc32d7449928304533d03a9e3cb3
권나연 <nykwon7777@gmail.com>
2026-05-28 15:12:56 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.02 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,43 +1,34 @@
 """
-상대팁 진영에 도착하기 위해서 지나야 하는 칸의 최솟값
+최소거리 -> bfs!
 
-도착 x -> -1 return!
-
-1이면 o, 
-
-상하좌우 살펴보고 -> 0이면 x, 
-
-0, 0 -> n, m으로 가야함. 
-
-bfs!
 """
 from collections import deque
 
 def solution(maps):
-    answer = -1
-    N, M = len(maps), len(maps[0])
-    # visited = [[0 for _ in range(M)] for _ in range(N)]
-    visited = [[0] * M for _ in range(N)]
+    rows = len(maps)
+    cols = len(maps[0])
+    
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
+    
+    visited = [[False for _ in range(cols)] for _ in range(rows)]
+    q = deque([(0, 0, 1)])
+    visited[0][0] = True
     
     def inRange(x, y):
-        return (x >= 0 and x < N and y >= 0 and y < M)
-    
-    dx = [1, 0, -1, 0]
-    dy = [0, 1, 0, -1]
-    
-    q = deque([(0, 0, 1)])
+        return 0<=x<rows and 0<=y<cols
     
     while q:
-        cur_x, cur_y, dis = q.popleft();
-        if (cur_x == N - 1 and cur_y == M - 1):
-            return dis
+        cur_x, cur_y, cost = q.popleft()
+        
+        if cur_x == rows - 1 and cur_y == cols - 1:
+            return cost
         
         for i in range(4):
             nx = cur_x + dx[i]
             ny = cur_y + dy[i]
             
-            if (inRange(nx, ny) and maps[nx][ny] and not visited[nx][ny]):
+            if inRange(nx, ny) and not visited[nx][ny] and maps[nx][ny] == 1:
                 visited[nx][ny] = True
-                q.append((nx, ny, dis + 1))
-    
-    return answer
+                q.append((nx, ny, cost + 1))
+    return -1
