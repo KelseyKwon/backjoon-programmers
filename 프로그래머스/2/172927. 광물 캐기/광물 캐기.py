2a0443d5f7fabce2e36d480192cce72114af832c
권나연 <nykwon7777@gmail.com>
2026-05-29 20:37:46 +0900

    [level 2] Title: 광물 캐기, Time: 0.02 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -1,59 +1,42 @@
 """
-사용할 수 없을 때까지 사용 (즉, 5번을 꼭 사용해야 함)
-주어진 순서대로만 캘 수 있다.
-end 조건 : 모든 광물을 캐거나, 더 사용할 곡괭이가 없을때까지
+0 다이아 1 iron 2 stone
 
-dia, iron, stone 순으로 되어 있음...
-
-cur_turn = 0
-+1 해. 그리고, cur_turn = 5이면, 그 다음으로 넘어가
-
-max_cur_turn = picks[i] * 5
-cur_turn = max_cur_turn -> 인덱스 하나 증가시켜
+현재 상황에서 가장 최선의 선택을 해야 한다. -> 그리디!
+순서는 상관 없다 -> 그리디!
 """
 
 def solution(picks, minerals):
     answer = 0
-    N = len(minerals)
+    pick_num = sum(picks)
+    if pick_num == 0:
+        return 0
+    
     info = []
     
-    able_pick = sum(picks) * 5
-    minerals = minerals[:able_pick]
-    # print(minerals)
+    minerals = minerals[:pick_num * 5]
+    for i in range(0, len(minerals), 5):
+        chunked = minerals[i:i+5]
+        
+        dia = chunked.count("diamond")
+        iron = chunked.count("iron")        
+        stone = chunked.count("stone")   
+        
+        info.append((dia, iron, stone))
     
-    # print(info)
-    for i in range(0, N, 5):
-        # diamond_nums = sum(mins == 'diamond' for mins in minerals[i:min(i+5, N)])
-        diamond_nums = minerals[i:min(i+5, N)].count('diamond')
-        iron_nums = minerals[i:min(i+5, N)].count('iron')
-        stone_nums = minerals[i:min(i+5, N)].count('stone')
-        info.append([diamond_nums, iron_nums, stone_nums])
-    # 다이아 개수를 기준으로 정렬
-    info.sort(key=lambda x: (-x[0], -x[1], -x[2]))
-    print(info)
+    info.sort(key=lambda x:(-x[0], -x[1], -x[2]))
     
-    # 0은 다이아 곡괭이, 1은 철 곡괭이
-    fatigue = [[1, 1, 1], [5, 1, 1], [25, 5, 1]]
-    cur_kok = 0
     
-    # i[0] i[1] i[2] 가 각각 다이아, 철, 돌
     for i in info:
-        # 모두 다 0이면 문제가 됨 -> 왜냐? 연속적으로 0일 수 있으므로
-        
-        # 1, 3, 2 별로 돈다. 
-        # if picks[cur_kok] == 0:
-        #     if cur_kok == 2:
-        #         break
-        #     cur_kok += 1
-        while cur_kok < 3 and picks[cur_kok] == 0:
-            cur_kok += 1
-        if cur_kok == 3:
-            break
-        dia_fatigue = i[0] * fatigue[cur_kok][0]
-        iron_fatigue = i[1] * fatigue[cur_kok][1]
-        stone_fatigue = i[2] * fatigue[cur_kok][2]
-        
-        picks[cur_kok] -= 1
-        answer += dia_fatigue + iron_fatigue + stone_fatigue
-    
+        dia, iron, stone = i
+        if picks[0]:
+            answer += dia + iron + stone
+            picks[0] -= 1
+        elif picks[1]:
+            answer += dia * 5 + iron + stone
+            picks[1] -= 1
+        else:
+            answer += dia * 25 + 5 * iron + stone
+            picks[2] -= 1
     return answer
+    
+    
