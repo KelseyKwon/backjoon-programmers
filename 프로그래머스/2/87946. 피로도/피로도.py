26f3957348edf9c29bb784fe74f88b83cb992dd6
권나연 <nykwon7777@gmail.com>
2026-05-29 23:36:51 +0900

    [level 2] Title: 피로도, Time: 21.38 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,27 +1,21 @@
 """
-모든 경우의 수를 다 따져 봐야 함.
+전체를 다 봐야돼! -> 백트래킹??
 
 """
 
 def solution(k, dungeons):
-    visited = [False for _ in range(len(dungeons) + 1)]
+    answer = 0
+    lens = len(dungeons)
+    visited = [False] * lens
     
-    def backtrack(cur_k, level):
-        max_level = level
-        if level == len(dungeons):
-            return max_level
-        
-        for i, d in enumerate(dungeons):
-            if not visited[i] and d[0] <= cur_k:
-                visited[i] = True
+    def backtrack(fatigue, count):
+        nonlocal answer
+        answer = max(count, answer)
+        for d in range(lens):
+            if not visited[d] and dungeons[d][0] <= fatigue:
+                visited[d] = True
+                backtrack(fatigue - dungeons[d][1], count+1)
+                visited[d] = False
                 
-                # 중요 ! 백트랙 -> 다음 단계로 넘어가고, 그 결과 중 가장 큰 값을 받아와서 갱신하기
-                result = backtrack(cur_k - d[1], level+1)
-                max_level = max(max_level, result)
-                
-                visited[i] = False
-        return max_level
-    
-    return backtrack(k, 0)
-    
-    
+    backtrack(k, 0)
+    return answer
