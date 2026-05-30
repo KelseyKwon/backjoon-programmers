e586d51d9ffeb9abefeeb3bc483dc24860c40393
권나연 <nykwon7777@gmail.com>
2026-05-30 21:32:14 +0900

    [level 3] Title: 네트워크, Time: 0.22 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,11 +1,14 @@
+"""
+일단 덩어리를 세야돼.
+"""
+
 def solution(n, computers):
     answer = 0
-    
     visited = [False] * n
     
     def dfs(cur):
         for k in range(n):
-            if computers[cur][k] and not visited[k]:
+            if not visited[k] and computers[cur][k] == 1:
                 visited[k] = True
                 dfs(k)
     
@@ -14,5 +17,4 @@ def solution(n, computers):
             visited[i] = True
             dfs(i)
             answer += 1
-    
     return answer
