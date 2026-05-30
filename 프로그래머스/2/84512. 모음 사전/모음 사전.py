cca05ca43be888641e65efba41bf350a1b39fc11
권나연 <nykwon7777@gmail.com>
2026-05-30 14:18:13 +0900

    [level 2] Title: 모음 사전, Time: 0.50 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,23 +1,26 @@
+"""
+백트래킹!
+"""
+
 def solution(word):
     count = 0
     answer = 0
-    characters = ['A', 'E', 'I', 'O', 'U']
-    def backtrack(cur):
+    
+    def backtrack(cur_word):
         nonlocal count, answer
+        
         if answer > 0:
             return
         
-        if cur == word:
+        if cur_word == word:
             answer = count
             return
         
-        if len(cur) >= 5:
-            return
+        if len(cur_word) >= 5: 
+            return 
         
-        for a in characters:
+        for a in ['A', 'E', 'I', 'O', 'U']:
             count += 1
-            backtrack(cur + a)
-            
+            backtrack(cur_word + a)
     backtrack("")
-    
     return answer
