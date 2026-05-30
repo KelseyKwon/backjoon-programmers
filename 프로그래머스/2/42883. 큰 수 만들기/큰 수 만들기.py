931b3c7dab8ce365bd78d16b90d7484c1f8d1728
권나연 <nykwon7777@gmail.com>
2026-05-30 23:58:13 +0900

    [level 2] Title: 큰 수 만들기, Time: 45.67 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,15 +1,18 @@
 """
-2개를 제거했을때 가장 큰 수 -> 그리디!
-1924에서 가장 작은 것을 빼면 됨. k개 만큼
+stack을 이용해보자!
+
+[1, 9, 2, 4]
+
 """
 
 def solution(number, k):
-    answer = ''
     stack = []
     
-    for s in range(len(number)):
-        while stack and k > 0 and stack[-1] < number[s]:
+    for i in range(len(number)):
+        while stack and k > 0 and stack[-1] < number[i]:
             stack.pop()
-            k-=1
-        stack.append(number[s])
-    return "".join(stack[:len(number)-k])
+            k -= 1
+        
+        stack.append(number[i])
+    
+    return "".join(stack[:len(number) - k])
