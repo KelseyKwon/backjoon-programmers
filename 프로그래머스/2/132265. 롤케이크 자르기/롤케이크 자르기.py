40f6870393fa3b3150ca184891e92547e65d81f8
권나연 <nykwon7777@gmail.com>
2026-05-30 12:28:37 +0900

    [level 2] Title: 롤케이크 자르기, Time: 249.52 ms, Memory: 51.6 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,31 +1,18 @@
-"""
-토핑이 중요!
-동일한 가짓수의 토핑
-
-0 ~ 3 3~ 마지막
-
-토핑이 동일하게 하는 법!
-
-모든 경우의수를 봐야 함
-백트래킹??
-"""
 from collections import Counter
 
 def solution(topping):
-    set_dict = Counter(topping)
     answer = 0
-    set_top = set()
     
-    for i in topping:
-        set_dict[i] -= 1
-        set_top.add(i)
-        if set_dict[i] == 0:
-            set_dict.pop(i)
+    top = Counter(topping)
+    new_top = set()
+    
+    for t in topping:
+        new_top.add(t)
+        top[t] -= 1
         
-        if len(set_top) == len(set_dict):
+        if top[t] == 0:
+            del top[t]
+        
+        if len(top) == len(new_top):
             answer += 1
     return answer
-    
-    
-        
-    
