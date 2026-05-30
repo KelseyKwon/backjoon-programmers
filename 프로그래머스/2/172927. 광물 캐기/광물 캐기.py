ccc05871f5bad7701ef718c60abec40fd7ea8762
권나연 <nykwon7777@gmail.com>
2026-05-31 08:45:01 +0900

    [level 2] Title: 광물 캐기, Time: 0.03 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -1,42 +1,43 @@
 """
-0 다이아 1 iron 2 stone
+5개 연속으로 캐니까 순서는 상관 없잖아
+그니까 5개 기준으로 해서 -> dia, iron, stone의 개수가 많은 순서대로 나열해
 
-현재 상황에서 가장 최선의 선택을 해야 한다. -> 그리디!
-순서는 상관 없다 -> 그리디!
+단 마지막은 무조건 마지막
 """
 
 def solution(picks, minerals):
     answer = 0
-    pick_num = sum(picks)
-    if pick_num == 0:
-        return 0
-    
+    pick_sum = sum(picks)
     info = []
     
-    minerals = minerals[:pick_num * 5]
+    minerals = minerals[:5 * pick_sum]
+    
     for i in range(0, len(minerals), 5):
-        chunked = minerals[i:i+5]
-        
-        dia = chunked.count("diamond")
-        iron = chunked.count("iron")        
-        stone = chunked.count("stone")   
+        cur_min = minerals[i:i+5]
+        dia = cur_min.count("diamond")
+        iron = cur_min.count("iron")
+        stone = cur_min.count("stone")
         
         info.append((dia, iron, stone))
     
-    info.sort(key=lambda x:(-x[0], -x[1], -x[2]))
-    
+    info.sort(key = lambda x : (-x[0], -x[1], -x[2]))
     
     for i in info:
         dia, iron, stone = i
         if picks[0]:
-            answer += dia + iron + stone
             picks[0] -= 1
+            answer += dia + iron + stone
         elif picks[1]:
-            answer += dia * 5 + iron + stone
             picks[1] -= 1
+            answer += dia * 5 + iron + stone
         else:
-            answer += dia * 25 + 5 * iron + stone
             picks[2] -= 1
+            answer += dia * 25 + iron * 5 + stone
+    
     return answer
+            
+            
     
     
+    
+        
