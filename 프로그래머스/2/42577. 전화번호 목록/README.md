5c1bd0d5dcd07ca0e8f2d7969a299929dc27a4d8
권나연 <nykwon7777@gmail.com>
2026-05-30 23:43:43 +0900

    [level 2] Title: 전화번호 목록, Time: 46.81 ms, Memory: 28.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 29.9 MB, 시간: 119.29 ms
+메모리: 28.7 MB, 시간: 46.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 07일 14:05:18
+2026년 05월 30일 23:43:41
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,26 +1,12 @@
-"""
-한 번호가 다른 번호의 접두어인지 확인하는법
-
-일단 배열을 순서대로 정렬하고
-
-119 1195524421
-
-뒤에 있는 원소가 직전에 있는 원소로 시작하면 false 반환
-"""
 
 def solution(phone_book):
-    answer = True
-    
     phone_book.sort()
-    prefix = phone_book[0].strip()
+    prev = "-1"
     
-    if (len(phone_book) == 1):
-        return True
-    
-    for i in range(1, len(phone_book)):
-        if (phone_book[i].strip().startswith(prefix)):
+    for phone in phone_book:
+        if phone.startswith(prev):
             return False
         else:
-            prefix = phone_book[i].strip()
-    
+            prev = phone
+        
     return True
